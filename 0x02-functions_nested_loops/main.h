4876d485d0eaf5c337180b80881e2468274b5144
Omar-KAhmed <omarkhaledahmedov7@gmail.com>
2023-02-21 21:30:56 +0100

    omar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-#include "../holberton.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+int _putchar(char c);
