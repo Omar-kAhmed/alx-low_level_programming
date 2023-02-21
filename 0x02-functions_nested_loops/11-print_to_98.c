5564b1078186db575d2d0bfdf6cc6f78af8be896
Omar-KAhmed <omarkhaledahmedov7@gmail.com>
2023-02-21 22:48:20 +0100

    omar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 /*
  * File: 11-print_to_98.c
- * Auth: Brennan D Baraban
+ * Auth: Omar Ahmed
  */
 
 #include <stdio.h>
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 /*
  * File: 7-print_last_digit.c
- * Auth: Brennan D Baraban
+ * Auth: Omar Ahmed
  */
 
 #include "holberton.h"
