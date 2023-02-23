e00d351dbb81f037da337d1fd806bcb8974945cf
Omar-KAhmed <omarkhaledahmedov7@gmail.com>
2023-02-23 18:39:35 +0100

    omar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
-void print_numbers(void) {
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
 
+void print_most_numbers(void)
+{
+	int c;
 
-int i;
-
-for (i=-1;i<=9;i++){
-
-
- _putchar(i);
- _putchar(\n);
-
-}
-return (0);
-
-
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
 }
