5a1be939345a1409a753aae91976429ccafce24c
Omar-KAhmed <omarkhaledahmedov7@gmail.com>
2023-02-23 17:19:23 +0100

    omar

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,35 @@
+/*
+*
+* File: 0-isupper.c
+*
+* Auth: Omar_KAhmed
+*
+*/
+
+
+#include <main.h>
+
+/*
+*
+* Main: Write a function that checks for uppercase character.
+*
+* Return: 1 if c is upper case; otherwise 0. 
+*
+*?
+
+
+int _isupper(int c) {
+
+
+if (c == [[:UPPER:]]) {
+
+ return 1;
+
+  else
+
+  return 0;
+}
+
+
+
+}
