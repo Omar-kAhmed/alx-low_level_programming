56de0031166de9a1a03a26521419ad48898503a2
Omar-KAhmed <omarkhaledahmedov7@gmail.com>
2023-02-23 18:26:42 +0100

    omar

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,35 @@
+/*
+*
+* File: 1-isdigit.c
+*
+* Auth: Omar_KAhmed
+*
+*/
+
+#include "main.h"
+
+
+/*
+*
+* Main: Write a function that checks for a digit (0 through 9).
+*
+* Return: 1 if c is a digit.
+* Return : 0 otherwise.
+*/
+
+
+int _isdigit (int c) {
+
+
+if (c >= 0 && c <= 9){
+
+ retrun (1);
+
+  else 
+    
+   return (0);
+
+}
+
+
+}
